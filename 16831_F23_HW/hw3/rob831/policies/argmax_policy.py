2984590e04d2b9c92da774e654f52342379c1436
VineetTambe <tambevineet@gmail.com>
2023-10-09 13:13:02 -0400

    added argmax policy

--- 16831_F23_HW/hw3/rob831/policies/argmax_policy.py
+++ 16831_F23_HW/hw3/rob831/policies/argmax_policy.py
@@ -1,8 +1,8 @@
 import numpy as np
+from rob831.infrastructure import pytorch_util as ptu
 
 
 class ArgMaxPolicy(object):
-
     def __init__(self, critic):
         self.critic = critic
 
@@ -11,7 +11,10 @@ class ArgMaxPolicy(object):
             observation = obs
         else:
             observation = obs[None]
-        
-        ## TODO return the action that maxinmizes the Q-value 
+
+        ## TODO return the action that maxinmizes the Q-value
         # at the current observation as the output
+        # TODO verify the axis = 1 bit
+        action = np.argmax(ptu.to_numpy(self.critic.q_net(observation)), axis=1)
+
         return action.squeeze()
